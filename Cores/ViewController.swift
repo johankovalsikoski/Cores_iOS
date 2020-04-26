8c474f725b813255ab1024c23d8c508409d61f57
Johan Kovalsikoski <johan.kovalsikoski@gmail.com>
2020-04-26 17:40:45 -0300

    Initial Commit

---
+++ Cores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cores
+//
+//  Created by Johan Kovalsikoski on 26/04/20.
+//  Copyright © 2020 Johan Kovalsikoski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
